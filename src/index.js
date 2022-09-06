c6a24430a6aaec83c2a0a4edfff08289b2d31800
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-09-06 13:48:25 +0100

    Added all components inside the the index .js commented out the App import.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,62 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+function AddPersonForm(props){
+  const [person, setPerson] = useState("");
+
+  function handleChange(e){
+    setPerson(e.target.value);
+  }
+
+  function handleSubmit(e){
+    props.handleSubmit(person);
+    setPerson('');
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit ={handleSubmit}>
+      <input type="text"
+        placeholder='New Contact Name'
+        onChange={handleChange}
+        value ="person"/>
+        <button type='submit'>Add New Contact</button>
+    </form>
+  );
+}
+
+function PeopleList(props){
+  const arr = props.data;
+  const listItems = arr.map((value, index) => <li key={index}>{ value }</li>);
+
+  return(<ul>{listItems}</ul>);
+}
+
+function ContactManager(props){
+  const [contacts, setContacts] = useState(props.data);
+
+  function addPerson(name){
+    setContacts([...contacts, name]);
+  }
+
+  return(
+    <div>
+      <AddPersonForm handleChange={addPerson}/>
+      <PeopleList data={contacts} />
+    </div>
+  );
+}
+
+const contacts = ["John Fontain", "Bilal Philips", "Yusuf Estes"]; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ContactManager data ={contacts}/>
   </React.StrictMode>
 );
 
