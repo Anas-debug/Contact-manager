0608cd23263ec1c549f3b2e17f3bbe1c97471604
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-09-06 17:53:35 +0100

    Setting up a data array

---
+++ src/components/ContactManager.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const list = [
+    {
+      id: 1,
+      firstname: 'Omar',
+      lastname: 'Chatila',
+      country: 'USA',
+    },
+    {
+      id: 2,
+      firstname: 'Zakir',
+      lastname: 'Naik',
+      country: 'Malysia',
+    },
+
+    {
+      id: 3,
+      firstname: 'Pierre',
+      lastname: 'Vogel',
+      country: 'Germany'
+    },
+    {
+      id: 3,
+      firstname: 'John',
+      lastname: 'Fontain',
+      country: 'United Kingdom'
+    }
+  ];
